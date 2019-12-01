23d00718dff071927768c0df4a13f9535c6da2e0
Caleb Boylan <cboylan@redhat.com>
2019-12-01 09:07:03 -0800

    Make things more "functional" for day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,25 +1,34 @@
 use std::fs;
 use std::env;
 use std::cmp::max;
+use std::iter::successors;
 
+// It would be cool if this could just return an iterator (Map) instead of
+// collecting the results, but alas my rust-foo is lacking and i cannot
+// appease the type and borrow checker
 fn load_file(filename: &str) -> Vec<i64> {
     fs::read_to_string(filename).expect("Reading file failed").lines().map(|x| x.parse::<i64>().expect("Failed to convert string to i64")).collect()
 }
 
-fn calculate_fuel(mass: i64) -> i64 {
-    max(0, mass / 3 - 2)
+fn calculate_fuel(mass: i64) -> Some(i64) {
+    max(0, mass / 3 - 2);
+}
+
+// Return the output of calculate_fuel, but as an Option, Some(x) if x > 0 else None
+// This makes doing calculate_fuel_fuel easier because it will stop on None
+fn calculate_fuel_fuel_helper(mass: i64) -> Some(i64) {
+    let val = calculate_fuel(mass)
+    if val > 0 {
+        Some(val)
+    } else {
+        None
+    }
 }
 
 // This calculates the fuel needed for the mass + the fuel needed for the fuel as is required for
 // part 2
 fn calculate_fuel_fuel(mass: i64) -> i64 {
-    let mut fuel_total = calculate_fuel(mass);
-    let mut fuel_to_add = calculate_fuel(fuel_total);
-    while fuel_to_add > 0 {
-        fuel_total += fuel_to_add;
-        fuel_to_add = calculate_fuel(fuel_to_add);
-    }
-    fuel_total
+    successors(calculate_fuel_helper(mass), |&x| calculate_fuel_helper(x)).fold(0, |sum, x| sum + x)
 }
 
 fn main() {
